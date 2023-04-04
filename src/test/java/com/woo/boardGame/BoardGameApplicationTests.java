450c98c7371c9114888169291dec6a3953ccfefe
woo <chlguddn7@gmail.com>
2023-04-04 15:15:35 +0900

    Initial commit

---
+++ src/test/java/com/woo/boardGame/BoardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woo.boardGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
